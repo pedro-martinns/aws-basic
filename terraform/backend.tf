0d3400da4e96f56f5cf8f507ce4efd98a6024348
pedro-martinns <pedro.mab.martins@outlook.com>
2025-08-04 21:21:48 +0100

    Update github workflow with working-directory

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key    = "terraform.tfstate"
     region = "eu-west-2"
   }
-  required_version = ">=1.12.2"
+  #required_version = ">=1.12.2"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
